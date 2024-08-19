dfcf6147457687157538b6e70950e00d94a53b2f
John Vu <hoanganh170788@gmail.com>
2024-08-19 14:31:32 +0700

    Fixed some syntax errors

--- script.sh
+++ script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Awesomewm install script
-# install xorg + rec app and shit
-sudo apt install -y xorg git unzip feh libpcre3-dev rofi imagemagick acpi bluez blueman xbacklight scrot i3lock nautilus xfce4-power-manager curl tar grep
+# Awesomewm & X11 install script on Debian 12 Minimal Install
+# install minimal xorg
+sudo apt install -y xserver-xorg-video-intel xserver-xorg-core xinit 
+
+# Rec app and shit
+sudo apt install -y git unzip feh libpcre3-dev rofi imagemagick acpi bluez blueman xbacklight scrot i3lock nautilus xfce4-power-manager curl tar grep gpg
+
+clear
 # install sddm
 sudo apt install --no-install-recommends -y sddm
 
@@ -21,28 +26,71 @@ cd picom-ibhagwan
 meson setup --buildtype=release build
 ninja -C build
 sudo ninja -C build install
-rm -rf build picom*
 
-## Fonts
+## Global Fonts
 mkdir req_font
 cd req_font
 wget -O req_font.zip "https://www.dropbox.com/scl/fo/3m2hyf95iwp2gl00xqduq/AJ6CGeJEsJ98t-r36LYziTI?rlkey=8mokcdahierso4ly01v1dh5l0&st=ulz6nooe&dl=1"
 unzip *.zip
-rm -rf *.zip
 sudo cp *.{ttf,otf} /usr/local/share/fonts
 sudo fc-cache
 cd
-rm -rf req_font
 
 ## Dotfiles
 sudo cp -a ~/dotfiles/. ~/.config/
 
+# oh-my-zsh
 clear
+echo "Do you want to install oh-my-zsh? (y/n)"
+read zsh_opt
+if [ "$zsh_opt" == "y" ]; then
+    echo "Do you want to install powerlevel10's ZSH theme and install some plugins? (y/n)"
+    read zsht_opt
+    sudo apt install -y zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    if [ "$zsht_opt" == "y" ]; then
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+        sudo cp ~/.zshrc ~/zshrc.bak
+        sudo sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$HOME/.zshrc"
+        sudo sed -i 's/^plugins=(git)$/plugins=(git, zsh-syntax-highlighting, zsh-autosuggestions)/' "$HOME/.zshrc"
+    fi
+else
+    echo "Won't install ZSH"
+fi
+
+# Spicetify
+printf "\n"
+echo "Do you want to install Spicetify? (y/n)"
+read spicetify_opt
+    if [ "$spicetify_opt" == "y" ]; then
+        ### Spotify
+        sudo mkdir -p /etc/apt/keyrings
+        curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+		echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+        echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+        sudo apt update
+        sudo apt install -y spotify-client
+
+        ### prompt for username to install spicetify
+        export PATH="$PATH:/home/$USER/.spicetify"
+        ### Spicetify
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh)"
+    else
+        echo "Won't install Spicetify"
+    fi
+
+
+clear
+
+echo "Cleanning up..."
+cd
+rm -rf req_font/ build/ picom*/ dotfiles/
+
 # Reboot
 echo "All Done. Reboot now? (y/n)"
 read reboot_opt
 if [ "$reboot_opt" == "y" ]; then
     sudo reboot
 else
-    echo "Please reboot later for shit to work"
+    echo "Please reboot later"
 fi
